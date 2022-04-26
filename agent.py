87d76513c571219d73654e0e3cebb48031c470a2
zhangxs <zhangxs7@smail.swufe.edu.cn>
2022-04-26 16:38:11 +0800

    agent

--- agent.py
+++ agent.py
@@ -1,1 +1,14 @@
+import random
+
+
+class agent:
+    def __init__(self, id, cash,shares=0,now_price,env):
+        self.id= id
+        self.cash = cash
+        self.env = env
+        self.shares = shares
+        self.now_price = now_price
+    def buy(self):
+        share = random.randint(1,10)
+        price =
 
