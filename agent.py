2f38f06aeac2623421cd49d69d57ad93f60a1628
zhangxs <zhangxs7@smail.swufe.edu.cn>
2022-04-26 17:39:20 +0800

    update

--- agent.py
+++ agent.py
@@ -6,9 +6,14 @@ class agent:
         self.id= id
         self.cash = cash
         self.env = env
-        self.shares = shares
+        self.shares = shares #持有股份
         self.now_price = now_price
     def buy(self):
-        share = random.randint(1,10)
-        price =
+        share = random.randint(1,10) #买1-10手
+        price = self.now_price+0.01
+        if self.cash<price*share*100:
+            print('{}余额不足，无法购买！'.format(self.id))
+        else:
+            action_buy = [share,price,self.id]
+        return action_buy
 
